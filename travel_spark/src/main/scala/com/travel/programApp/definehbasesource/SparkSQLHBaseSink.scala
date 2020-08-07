c28e7a77aa2a8245fc66a555435afd8a700f7af6
Crazy_kevin <kevin-me@outlook.com>
2020-08-07 17:24:05 +0800

    spark自定义hbase的数据源

---
+++ travel_spark/src/main/scala/com/travel/programApp/definehbasesource/SparkSQLHBaseSink.scala
@@ -0,0 +1,14 @@
+package com.travel.programApp.definehbasesource
+
+import org.apache.spark.sql.{DataFrame, SaveMode}
+
+object SparkSQLHBaseSink {
+  def saveToHBase(dataFrame: DataFrame, tableName: String, rowkey: String, fields: String): Unit = {
+    dataFrame.write.format("com.travel.programApp.hbaseSink.HBaseSink")
+      .mode(SaveMode.Overwrite)
+      .option("hbase.table.name", tableName)
+      .option("hbase.rowkey", rowkey)
+      .option("hbase.fields", fields)
+      .save()
+  }
+}
